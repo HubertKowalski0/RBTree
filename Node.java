5696e48409d22aabb227c85f2e90f57681e21431
HubertKowalski0 <155483229+HubertKowalski0@users.noreply.github.com>
2024-04-18 13:17:42 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,16 @@
+public class Node public class Node {
+    int key;
+    int value;
+
+    Node left;
+    Node right;
+    Node parent;
+
+    boolean black;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}{
+}
